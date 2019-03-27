984c6f1c70ffe1545dcb365fd06593ababdbac0a
christinegithub <christine13@gmail.com>
2019-03-26 23:44:32 -0400

    light switch works

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
 
+document.addEventListener('DOMContentLoaded', function() {
 
+  var lightswtich = document.getElementsByTagName('button')[0];
+
+  lightswtich.addEventListener('click', pushSwitch)
+
+  function pushSwitch() {
+    var text = document.getElementsByTagName('h1')[0];
+    var body = document.getElementsByTagName('body')[0];
+
+
+    if (body.classList.contains("light")) {
+      body.classList.remove("light");
+      body.classList.add("dark");
+      text.innerText = "Hey, who turned off the lights?";
+      lightswtich.classList.remove("on");
+      lightswitch.classList.add("off");
+
+    } else {
+      body.classList.remove("dark");
+      body.classList.add("light");
+      text.innerText = "It's so bright in here!";
+      lightswitch.classList.add("on");
+      lightswitch.classList.remove("off");
+    }
+  }
+})
